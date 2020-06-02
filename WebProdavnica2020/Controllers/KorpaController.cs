f4cdf8691693cca115723c81424b3c4bd8e2b94f
bajicdusan <sopotssopot@gmail.com>
2020-06-02 17:30:20 +0200

    Added ShoppingCard - Korpa

---
+++ WebProdavnica2020/Controllers/KorpaController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebProdavnica2020.Models;
+using WebProdavnica2020.Servisi;
+
+namespace WebProdavnica2020.Controllers
+{
+    public class KorpaController : Controller
+    {
+        private readonly KorpaServis ks;
+        private Korpa k;
+        private ProdavnicadbContext db;
+
+        public KorpaController(ProdavnicadbContext _db, KorpaServis _ks)
+        {
+            db = _db;
+            ks = _ks;
+            k = ks.CitajKorpu();
+        }
+        public IActionResult Index()
+        {
+            return View(k);
+        }
+
+
+        public IActionResult DodajStavku(int ProizvodId)
+        {
+            Proizvod p1 = db.Proizvodi.Find(ProizvodId);
+
+            if (p1 != null)
+            {
+                k.DodajStavku(p1, 1);
+                ks.CuvajKorpu(k);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        public IActionResult PromeniStavku(int ProizvodId, int kolicina)
+        {
+            Proizvod p1 = db.Proizvodi.Find(ProizvodId);
+            if (p1 != null)
+            {
+                k.PromeniStavku(p1, kolicina);
+                ks.CuvajKorpu(k);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ObrisiStavku(int ProizvodId)
+        {
+            Proizvod p1 = db.Proizvodi.Find(ProizvodId);
+
+            if (p1 != null)
+            {
+                k.ObrisiStavku(p1.ProizvodId);
+                ks.CuvajKorpu(k);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
